713ef8ff24593366c5714e3ee1761dcb910ad3ee
matiass-msa <saraviaarevalomatias@pedropoveda.edu.bo>
2026-04-09 14:33:29 -0400

    Update creartarea.php
    estilos

--- creartarea.php
+++ creartarea.php
@@ -17,24 +17,29 @@
 }
 
 .tra{
-  background: #2536eb;
-  padding:20px;
-  border-radius:10px;
-  width:280px;
-  text-align:center;
+  background: #ffffff;
+      padding:25px;
+      border-radius:15px;
+      width:320px;
+      text-align:center;
+      box-shadow: 0 10px 25px rgba(0,0,0,0.2);
+      animation: fadeIn 0.8s ease-in-out;
 }
 
 input, select, button{
   width:100%;
-  margin:8px 0;
-  padding:8px;
-  border-radius:5px;
-  border:1px solid blue;
+      margin:10px 0;
+      padding:10px;
+      border-radius:8px;
+      border:1px solid #ccc;
+      outline:none;
+      transition:0.3s;
+      font-size:14px;
 }
 
 button{
   background:#4facfe;
-  color:white;
+  color:red;
 }
   </style>
 <body>
